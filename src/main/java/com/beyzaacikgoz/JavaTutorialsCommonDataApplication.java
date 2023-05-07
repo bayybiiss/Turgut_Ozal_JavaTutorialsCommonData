301e633a37e6870c579b5760a6ec572de8986e4f
beyza ackgz <beyza-ecem2001@hotmail.com>
2023-05-07 14:42:27 +0300

    All common data added

--- src/main/java/com/beyzaacikgoz/JavaTutorialsCommonDataApplication.java
+++ src/main/java/com/beyzaacikgoz/JavaTutorialsCommonDataApplication.java
@@ -1,13 +1,28 @@
 package com.beyzaacikgoz;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import java.util.TimeZone;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+})
+
+//@SpringBootApplication
 public class JavaTutorialsCommonDataApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JavaTutorialsCommonDataApplication.class, args);
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
     }
 
-}
+    //PSVM
+    public static void main(String[] args) {
+        //Disables headless JOptionPane
+        System.setProperty("java.awt.headless","false");
+
+        SpringApplication.run(JavaTutorialsCommonDataApplication.class, args);
+    }//PSVM END
+}//END CLASS
---
+++ src/main/java/com/beyzaacikgoz/persist/DataPersist.java
@@ -0,0 +1,5 @@
+package com.beyzaacikgoz.persist;
+
+public class DataPersist {
+    //URL https://github.com/bayybiiss/Turgut_Ozal_JavaTutorialsCommonData
+}
