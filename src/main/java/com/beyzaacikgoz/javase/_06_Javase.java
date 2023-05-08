dbe54752a3e51353f26ef0c1237d41e4e42cfe13
beyza ackgz <beyza-ecem2001@hotmail.com>
2023-05-08 12:55:29 +0300

    All common data added

--- src/main/java/com/beyzaacikgoz/javase/_06_Javase.java
+++ src/main/java/com/beyzaacikgoz/javase/_06_Javase.java
@@ -1,4 +1,35 @@
 package com.beyzaacikgoz.javase;
 
 public class _06_Javase {
+    //Loop--Döngüler
+
+    public static void loopFor(){
+        for (int i=1; i<=10; i++){
+            System.out.print(i+" ");
+        }
+    }
+
+    public static void loopWhile() {
+        int i=1;
+        while (i<=10){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+
+    public static void loopDoWhile(){
+        int i=1;
+        do {
+            System.out.print(i+" ");
+            i++;
+        }while ( i<= 10);
+    }
+    public static void main(String[] args) {
+        loopFor();
+        System.out.println();
+        loopWhile();
+        System.out.println();
+        loopDoWhile();
+    }
+
 }
